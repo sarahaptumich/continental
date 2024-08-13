a41157d6e84be11414f68335302c7a6210482d6a
Sara Haptonstall <112976778+sarahaptumich@users.noreply.github.com>
2024-08-13 17:25:41 -0500

    Update rules.py

--- rules.py
+++ rules.py
@@ -5,4 +5,6 @@ def show_rules():
     st.write("""
     Continental is a game of combining cards like Gin Rummy, Remigio or Canasta. It has two peculiarities that make it unique: the plays that must be made after 
     each deal are fixed and it is possible to take another player's discard out of turn, taking another extra or "punishment" card when doing so.
-    """)
+    """
+    st.subheader(""" Game objective""", divider= "red")     
+            )
