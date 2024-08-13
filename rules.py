7a2c90a85b37a447794e191cfd756b9326f9e177
Sara Haptonstall <112976778+sarahaptumich@users.noreply.github.com>
2024-08-13 17:23:37 -0500

    Update rules.py

--- rules.py
+++ rules.py
@@ -3,12 +3,6 @@ import streamlit as st
 def show_rules():
     st.title("Game Rules")
     st.write("""
-    Here are the rules of the game:
-    1. The game is played between two players.
-    2. Players take turns to make a move.
-    3. The objective of the game is to achieve a specific goal.
-    4. The player who reaches the goal first wins the game.
-    5. If all moves are exhausted without a winner, the game ends in a draw.
-    
-    You can customize these rules based on the specific game you are developing.
+    Continental is a game of combining cards like Gin Rummy, Remigio or Canasta. It has two peculiarities that make it unique: the plays that must be made after 
+    each deal are fixed and it is possible to take another player's discard out of turn, taking another extra or "punishment" card when doing so.
     """)
